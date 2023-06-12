f63095f9ec168f4296cbbef7ce97ba8b9bfdc8ea
Marc Garcia <marc.ballester@nexum.com>
2023-06-12 14:51:23 +0200

    change stckbit config

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,44 +1,14 @@
 import { ContentfulContentSource } from '@stackbit/cms-contentful';
 import { defineStackbitConfig } from '@stackbit/types';
 
-const config = {
+export default defineStackbitConfig({
   stackbitVersion: '~0.6.0',
-  ssgName: 'nextjs',
-  nodeVersion: '16',
-  pagesDir: 'content/pages',
-  dataDir: 'content/data',
-  presetSource: {
-    types: 'files',
-    presetDirs: ['sources/local/presets'],
-  },
-  assets: {
-    referenceType: 'static',
-    staticDir: 'public',
-    uploadDir: 'images',
-    publicPath: '/',
-  },
-  pageLayoutKey: 'type',
-  styleObjectModelName: 'ThemeStyle',
   contentSources: [
     new ContentfulContentSource({
-      spaceId: process.env.CONTENTFUL_SPACE_ID || 'master',
-      environment: process.env.CONTENTFUL_ENVIRONMENT || 'master',
-      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN || 'master',
-      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN || 'master',
+      spaceId: process.env.CONTENTFUL_SPACE_ID!,
+      environment: process.env.CONTENTFUL_ENVIRONMENT!,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN!,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!,
     }),
   ],
-  modelExtensions: [{ name: 'page', type: 'page', urlPath: '/{slug}' }],
-  // Needed only for importing this repository via https://app.stackbit.com/import?mode=duplicate
-  import: {
-    type: 'contentful',
-    contentFile: 'contentful/export.json',
-    uploadAssets: true,
-    assetsDirectory: 'contentful',
-    spaceIdEnvVar: 'CONTENTFUL_SPACE_ID',
-    deliveryTokenEnvVar: 'CONTENTFUL_DELIVERY_TOKEN',
-    previewTokenEnvVar: 'CONTENTFUL_PREVIEW_TOKEN',
-    accessTokenEnvVar: 'CONTENTFUL_MANAGEMENT_TOKEN',
-  },
-};
-
-export default config;
+});
